ae4cd719bc26abad4566390626ff4331e3bfbfc9
trickbz <trickbz@gmail.com>
2016-04-02 11:23:12 +0300

    basic generate board functionality

--- app/components/configuration/controllers/configurationController.js
+++ app/components/configuration/controllers/configurationController.js
@@ -2,10 +2,47 @@
   angular
     .module('appModule')
     .controller('ConfigurationController', function ($scope) {
-      $scope.boardWidth = 4;
-      $scope.boardHeight = 4;
+      // will be taked from server using service
+      $scope.defaultBoardWidth = 4;
+      $scope.defaultBoardHeight = 4;
+
+      $scope.boardWidth = $scope.defaultBoardWidth;
+      $scope.boardHeight = $scope.defaultBoardHeight;
+
+      $scope.$watchGroup(['boardWidth', 'boardHeight'], function (newValue, oldValue) {
+        $scope.randomizeBoard();
+      });
+
+      $scope.generateSolvedBoardArray = function () {
+        var fieldsCount = $scope.boardHeight * $scope.boardWidth;
+        return _.map(new Array(fieldsCount), function (item, index) {
+          return index == fieldsCount - 1 ?
+            0: index + 1;
+        });
+      }
+
       $scope.arrayFromNumber = function (number) {
         return new Array(number);
       }
+
+      $scope.resetBoardSizeToDefault = function () {
+        $scope.boardWidth = $scope.defaultBoardWidth;
+        $scope.boardHeight = $scope.defaultBoardHeight;
+      }
+
+      $scope.isDefaultBoardSize = function () {
+        return $scope.boardWidth == $scope.defaultBoardWidth && $scope.boardHeight == $scope.defaultBoardHeight;
+      }
+
+      $scope.ordinalFieldNumberFromIndexes = function (indexHeight, indexWidth) {
+        return $scope.boardWidth * indexHeight + indexWidth;
+      }
+
+      $scope.randomizeBoard = function () {
+        var solvedArray = $scope.generateSolvedBoardArray();
+        $scope.board = _.shuffle(solvedArray);
+      }
+
+      $scope.randomizeBoard();
     });
 })();
